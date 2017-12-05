17afc1b89a49e229da2078c6f87ca49e9486a7a0
Arnold Christian Veloso Lima <arnoldcvl@gmail.com>
2017-12-05 18:33:05 -0200

    Código do Banco atualizado 5/12
    Simplificação do Banco

--- library.sql
+++ library.sql
@@ -1,93 +1,267 @@
-CREATE DATABASE Biblioteca;
+-- phpMyAdmin SQL Dump
+-- version 4.7.6
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: Dec 05, 2017 at 08:27 PM
+-- Server version: 5.6.38
+-- PHP Version: 7.1.9
 
-USE Biblioteca;
-CREATE TABLE Editora
-(
-  idEditora INT NOT NULL,
-  Nome VARCHAR(128) NOT NULL,
-  Endereço VARCHAR(256) NOT NULL,
-  Telefone VARCHAR(9) NOT NULL,
-  PRIMARY KEY (idEditora)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE Cliente
-(
-  Matrícula INT NOT NULL,
-  Nome VARCHAR(128) NOT NULL,
-  Telefone_Pessoal VARCHAR(9) NOT NULL,
-  idCurso INT,
-  idDepartamento INT,
-  idEmpresa INT,
-  PRIMARY KEY (Matrícula)
-);
 
-CREATE TABLE Biblioteca
-(
-  Campus VARCHAR(128) NOT NULL,
-  Endereco VARCHAR(258) NOT NULL,
-  idBiblioteca INT NOT NULL,
-  PRIMARY KEY (idBiblioteca)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE Acervo
-(
-  idAcervo INT NOT NULL,
-  idBiblioteca INT NOT NULL,
-  PRIMARY KEY (idAcervo),
-  FOREIGN KEY (idBiblioteca) REFERENCES Biblioteca(idBiblioteca)
-);
+--
+-- Database: `biblioteca`
+--
 
-CREATE TABLE Impresso
-(
-  idImpresso INT NOT NULL,
-  Título VARCHAR(128) NOT NULL,
-  dataPublicacao DATE NOT NULL,
-  Tipo INT NOT NULL,
-  idEditora INT NOT NULL,
-  idAcervo INT NOT NULL,
-  PRIMARY KEY (idImpresso),
-  FOREIGN KEY (idEditora) REFERENCES Editora(idEditora),
-  FOREIGN KEY (idAcervo) REFERENCES Acervo(idAcervo)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE Exemplar
-(
-  idExemplar INT NOT NULL,
-  idImpresso INT NOT NULL,
-  disponivel BOOLEAN NOT NULL,
-  PRIMARY KEY (idExemplar),
-  FOREIGN KEY (idImpresso) REFERENCES Impresso(idImpresso)
-);
+--
+-- Table structure for table `Acervo`
+--
 
-CREATE TABLE Retirada
-(
-  idRetirada INT NOT NULL,
-  DataRetirada DATE NOT NULL,
-  DataDevolucao DATE,
-  Devolvido BOOLEAN NOT NULL,
-  idExemplar INT NOT NULL,
-  Matrícula INT NOT NULL,
-  PRIMARY KEY (idRetirada, idExemplar),
-  FOREIGN KEY (idExemplar) REFERENCES Exemplar(idExemplar),
-  FOREIGN KEY (Matrícula) REFERENCES Cliente(Matrícula)
-);
+CREATE TABLE `Acervo` (
+  `idAcervo` int(11) NOT NULL,
+  `NomeAcervo` varchar(64) NOT NULL,
+  `EndereçoAcervo` varchar(64) NOT NULL DEFAULT 'Desconhecido'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE Reserva
-(
-  idReserva INT NOT NULL,
-  dataReserva DATE NOT NULL,
-  idExemplar INT NOT NULL,
-  Matrícula INT NOT NULL,
-  Reservado BOOLEAN NOT NULL,
-  PRIMARY KEY (idExemplar),
-  FOREIGN KEY (idExemplar) REFERENCES Exemplar(idExemplar),
-  FOREIGN KEY (Matrícula) REFERENCES Cliente(Matrícula)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE Escrito_por
-(
-  Nome VARCHAR(128) NOT NULL,
-  idImpresso INT NOT NULL,
-  PRIMARY KEY (Nome, idImpresso),
-  FOREIGN KEY (idImpresso) REFERENCES Impresso(idImpresso)
-);
+--
+-- Table structure for table `Cliente`
+--
+
+CREATE TABLE `Cliente` (
+  `Matrícula` int(11) NOT NULL,
+  `Senha` varchar(10) NOT NULL,
+  `Nome` varchar(128) NOT NULL,
+  `Telefone_Pessoal` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Exemplar`
+--
+
+CREATE TABLE `Exemplar` (
+  `idExemplar` int(11) NOT NULL,
+  `idImpresso` int(11) NOT NULL,
+  `idAcervo` int(11) NOT NULL,
+  `disponivel` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Impresso`
+--
+
+CREATE TABLE `Impresso` (
+  `idImpresso` int(11) NOT NULL,
+  `Título` varchar(128) NOT NULL,
+  `dataPublicacao` date NOT NULL,
+  `Tipo` int(11) NOT NULL,
+  `Editora` varchar(64) NOT NULL DEFAULT 'Desconhecido',
+  `Autor` varchar(256) NOT NULL DEFAULT 'Desconhecido'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Impresso`
+--
+
+INSERT INTO `Impresso` (`idImpresso`, `Título`, `dataPublicacao`, `Tipo`, `Editora`, `Autor`) VALUES
+(1, 'Padrões de Projeto', '2000-11-01', 1, 'Desconhecido', 'GAMMA, Erich');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Reserva`
+--
+
+CREATE TABLE `Reserva` (
+  `idReserva` int(11) NOT NULL,
+  `Matrícula` int(11) NOT NULL,
+  `idExemplar` int(11) NOT NULL,
+  `dataReserva` date NOT NULL,
+  `Reservado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Retirada`
+--
+
+CREATE TABLE `Retirada` (
+  `idRetirada` int(11) NOT NULL,
+  `Matrícula` int(11) NOT NULL,
+  `idExemplar` int(11) NOT NULL,
+  `DataRetirada` date NOT NULL,
+  `DataDevolucao` date DEFAULT NULL,
+  `Devolvido` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Tipo`
+--
+
+CREATE TABLE `Tipo` (
+  `idTipo` int(11) NOT NULL,
+  `TipoNome` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Tipo`
+--
+
+INSERT INTO `Tipo` (`idTipo`, `TipoNome`) VALUES
+(1, 'Livro'),
+(2, 'Revista'),
+(3, 'Periódico');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Acervo`
+--
+ALTER TABLE `Acervo`
+  ADD PRIMARY KEY (`idAcervo`);
+
+--
+-- Indexes for table `Cliente`
+--
+ALTER TABLE `Cliente`
+  ADD PRIMARY KEY (`Matrícula`);
+
+--
+-- Indexes for table `Exemplar`
+--
+ALTER TABLE `Exemplar`
+  ADD PRIMARY KEY (`idExemplar`),
+  ADD KEY `idImpresso` (`idImpresso`),
+  ADD KEY `idAcervo` (`idAcervo`);
+
+--
+-- Indexes for table `Impresso`
+--
+ALTER TABLE `Impresso`
+  ADD PRIMARY KEY (`idImpresso`),
+  ADD KEY `Tipo` (`Tipo`);
+
+--
+-- Indexes for table `Reserva`
+--
+ALTER TABLE `Reserva`
+  ADD PRIMARY KEY (`idReserva`),
+  ADD KEY `Matrícula` (`Matrícula`),
+  ADD KEY `idExemplar` (`idExemplar`);
+
+--
+-- Indexes for table `Retirada`
+--
+ALTER TABLE `Retirada`
+  ADD PRIMARY KEY (`idRetirada`) USING BTREE,
+  ADD KEY `idExemplar` (`idExemplar`),
+  ADD KEY `Matrícula` (`Matrícula`);
+
+--
+-- Indexes for table `Tipo`
+--
+ALTER TABLE `Tipo`
+  ADD PRIMARY KEY (`idTipo`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Acervo`
+--
+ALTER TABLE `Acervo`
+  MODIFY `idAcervo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Cliente`
+--
+ALTER TABLE `Cliente`
+  MODIFY `Matrícula` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Exemplar`
+--
+ALTER TABLE `Exemplar`
+  MODIFY `idExemplar` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Impresso`
+--
+ALTER TABLE `Impresso`
+  MODIFY `idImpresso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `Reserva`
+--
+ALTER TABLE `Reserva`
+  MODIFY `idReserva` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Retirada`
+--
+ALTER TABLE `Retirada`
+  MODIFY `idRetirada` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Tipo`
+--
+ALTER TABLE `Tipo`
+  MODIFY `idTipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Exemplar`
+--
+ALTER TABLE `Exemplar`
+  ADD CONSTRAINT `Exemplar_ibfk_2` FOREIGN KEY (`idAcervo`) REFERENCES `Acervo` (`idAcervo`),
+  ADD CONSTRAINT `Exemplar_ibfk_3` FOREIGN KEY (`idImpresso`) REFERENCES `Impresso` (`idImpresso`);
+
+--
+-- Constraints for table `Impresso`
+--
+ALTER TABLE `Impresso`
+  ADD CONSTRAINT `Impresso_ibfk_3` FOREIGN KEY (`Tipo`) REFERENCES `Tipo` (`idTipo`);
+
+--
+-- Constraints for table `Reserva`
+--
+ALTER TABLE `Reserva`
+  ADD CONSTRAINT `Reserva_ibfk_1` FOREIGN KEY (`Matrícula`) REFERENCES `Cliente` (`Matrícula`),
+  ADD CONSTRAINT `Reserva_ibfk_2` FOREIGN KEY (`idExemplar`) REFERENCES `Exemplar` (`idExemplar`);
+
+--
+-- Constraints for table `Retirada`
+--
+ALTER TABLE `Retirada`
+  ADD CONSTRAINT `Retirada_ibfk_1` FOREIGN KEY (`Matrícula`) REFERENCES `Cliente` (`Matrícula`),
+  ADD CONSTRAINT `Retirada_ibfk_2` FOREIGN KEY (`idExemplar`) REFERENCES `Exemplar` (`idExemplar`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
