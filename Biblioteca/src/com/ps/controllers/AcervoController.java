032a7da7bad0c6adfaf96cb254f981bb7e63a9e3
knowrafa <noprafa@gmail.com>
2017-12-05 22:39:44 -0200

    Conexão entre Services

--- Biblioteca/src/com/ps/controllers/AcervoController.java
+++ Biblioteca/src/com/ps/controllers/AcervoController.java
@@ -1,5 +1,6 @@
 package com.ps.controllers;
-
+import com.ps.models.ImpressoService;
+import java.util.Date;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,4 +13,8 @@ package com.ps.controllers;
  */
 public class AcervoController {
     
+    public AcervoController(int id, String titulo, String editora, Date dataPublicacao, String autores, String tipo){
+        ImpressoService impresso = new ImpressoService(id, titulo, editora, dataPublicacao, autores, tipo);
+        
+    }
 }
