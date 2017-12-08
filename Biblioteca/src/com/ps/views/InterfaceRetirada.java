9540f67bcd0ac2c5736e07328787e515299f6382
knowrafa <noprafa@gmail.com>
2017-12-07 23:51:09 -0200

    Interface Retirada Implementada
    "Implementada"

--- Biblioteca/src/com/ps/views/InterfaceRetirada.java
+++ Biblioteca/src/com/ps/views/InterfaceRetirada.java
@@ -23,7 +23,7 @@ import static sun.security.jgss.GSSUtil.login;
  *
  * @author elrafa
  */
-public class InterfacePesquisa extends javax.swing.JFrame {
+public class InterfaceRetirada extends javax.swing.JFrame {
     ArrayList<String[]> lista = new ArrayList<>();
     String[] nomesColunas = {"Nome do Impresso", "Autor", "Quantidade"};
     Boolean isOrNot = false;
@@ -31,12 +31,12 @@ public class InterfacePesquisa extends javax.swing.JFrame {
     /**
      * Creates new form InterfacePesquisa
      */
-    public InterfacePesquisa() {
+    public InterfaceRetirada() {
         initComponents();
         InitTable();
     }
     
-    public InterfacePesquisa(int matricula) {
+    public InterfaceRetirada(int matricula) {
         this.matricula = matricula;
         initComponents();
         InitTable();
@@ -67,9 +67,7 @@ public class InterfacePesquisa extends javax.swing.JFrame {
         jTextField2 = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -99,23 +97,9 @@ public class InterfacePesquisa extends javax.swing.JFrame {
 
         jLabel2.setText("Insira o Id do impresso");
 
-        jLabel3.setText("EMPRÉSTIMO");
-
-        jButton2.setText("Solicitar");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
+        jLabel3.setText("DEVOLUÇÃO");
 
         jButton3.setText("Devolver Livro");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
-            }
-        });
-
-        jLabel4.setText("DEVOLUÇÃO");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -136,24 +120,19 @@ public class InterfacePesquisa extends javax.swing.JFrame {
                         .addComponent(jLabel1)
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
             .addGroup(layout.createSequentialGroup()
-                .addGap(40, 40, 40)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(81, 81, 81)
-                        .addComponent(jLabel3)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jLabel4)
-                        .addGap(270, 270, 270))
                     .addGroup(layout.createSequentialGroup()
+                        .addGap(335, 335, 335)
+                        .addComponent(jLabel3))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(195, 195, 195)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel2)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(28, 28, 28)
-                                .addComponent(jButton2)
-                                .addGap(184, 184, 184)
-                                .addComponent(jButton3)))
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                                .addGap(58, 58, 58)
+                                .addComponent(jButton3)))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -167,15 +146,12 @@ public class InterfacePesquisa extends javax.swing.JFrame {
                 .addGap(32, 32, 32)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 214, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel4))
+                .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2)
                     .addComponent(jButton3))
                 .addGap(21, 21, 21))
         );
@@ -244,21 +220,6 @@ public class InterfacePesquisa extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jTable1MousePressed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        ImpressoController ic = new ImpressoController();
-        
-        if(ic.emprestaImpresso(Integer.parseInt(jTextField2.getText()), matricula)){
-            
-        }        
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        this.dispose();
-        InterfaceRetirada iretirada = new InterfaceRetirada();
-        iretirada.show();
-        
-    }//GEN-LAST:event_jButton3ActionPerformed
-
     
     
     /**
@@ -298,12 +259,10 @@ public class InterfacePesquisa extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
