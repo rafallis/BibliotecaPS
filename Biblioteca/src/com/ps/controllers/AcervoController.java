6bd853f6ce6fbb15b6e61fb8437b0790d1c41a8d
knowrafa <noprafa@gmail.com>
2017-12-12 19:38:24 -0200

    Essa aqui que é a correção MVC

--- Biblioteca/src/com/ps/controllers/AcervoController.java
+++ Biblioteca/src/com/ps/controllers/AcervoController.java
@@ -1,6 +1,7 @@
 package com.ps.controllers;
 import com.ps.controllers.ImpressoController;
 import com.ps.models.services.AcervoService;
+import com.ps.models.services.ImpressoService;
 import java.util.Date;
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,10 +15,8 @@ import java.util.Date;
  */
 public class AcervoController {
     
-    public void adicionaAoAcervo(int id, String titulo, String editora, Date dataPublicacao, String autores, String tipo){
-        ImpressoController impresso = new ImpressoController();
-        
-        impresso.adicionaImpresso(id, titulo, editora, dataPublicacao, autores, tipo);     
+     public void adicionaAoAcervo(int id, String titulo, String editora, Date dataPublicacao, String autores, String tipo){
+        ImpressoService impresso = new ImpressoService(id, titulo, editora, dataPublicacao, autores, tipo);
     }
     
 }
